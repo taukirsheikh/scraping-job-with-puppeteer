27658ea8f102286969a155da5e67f4eaf658a372
taukirsheikh <taukirsheikh2@gmail.com>
2024-05-07 17:23:48 +0545

    scraped jobs from Merojob

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hellow')
+const puppeteer = require('puppeteer');
+
+
+async function run() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://merojob.com/search/?q=developer');
+
+    const jobs = await page.evaluate(() => {
+        const jobCards = Array.from(document.querySelectorAll('.job-card'));
+        return jobCards.map(card => ({
+            'job_position': card.querySelector('a').title,
+            'company': card.querySelector('h3').title,
+            'link': card.querySelector('a').href,
+        })); 
+    });
+
+    console.log(jobs)
+
+    await browser.close();
+}
+
+run();
